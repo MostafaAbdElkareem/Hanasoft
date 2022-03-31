31e3557de7fbc583e17a9b28ee5077ca3c3de1a1
mostafa.abdelkareem@hotmail.com <mostafa.abdelkareem@hotmail.com>
2022-03-31 12:22:47 +1100

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from 'react';
 import Dashboard from './Pages/Dashboard';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark'
+	}
+});
+
 function App() {
-	return <Dashboard />;
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Dashboard />
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/Components/TabCharts/TabCharts.js
+++ src/Components/TabCharts/TabCharts.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import Chart from './Chart';
+import React, { useEffect, useState } from 'react';
+import ApexChart from './Chart';
 
 function TabCharts() {
+	const [ data, updateData ] = useState([ 1, 2, 3, 4 ]);
+	/* const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'cryptodeck.p.rapidapi.com',
+			'X-RapidAPI-Key': 'a174c60682msh130eee9f8fafb06p1042a9jsncd8345f803c1'
+		}
+	}; */
+	useEffect(
+		() => {
+			setInterval(() => {
+				const val = Math.floor(Math.random() * (100 - 30 + 1)) + 30;
+				let array = [ ...data, val ];
+				array.shift();
+				updateData(array);
+			}, 2000);
+		},
+		[ data ]
+	);
 	return (
 		<div>
 			Tab Charts
-			<Chart />
+			<ApexChart data={data} />
 		</div>
 	);
 }
